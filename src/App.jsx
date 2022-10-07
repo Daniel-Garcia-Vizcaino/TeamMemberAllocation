8e4b2714b10a894d753b8cf4216d322d027e6f70
Daniel Garcia Vizcaino <35636612+Daniel-Garcia-Vizcaino@users.noreply.github.com>
2022-10-07 19:44:29 +0000

    Add router

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './Header';
 import Employees from './Employees';
 import Footer from './Footer';
 import { useState, useEffect } from "react";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
 
@@ -119,15 +120,24 @@ function App() {
   
   return (
     <div>
-      <Header selectedTeam={selectedTeam}
-              teamMemberCount={employees.filter((employee) => employee.teamName === selectedTeam).length}
-      />
-      <Employees employees={employees}
-        selectedTeam={selectedTeam}
-        handleEmployeeCardClick={handleEmployeeCardClick}
-        handleTeamSelectionChange={handleTeamSelectionChange}
-      />
-      <Footer />
+      <Router>
+        <Header selectedTeam={selectedTeam}
+                teamMemberCount={employees.filter((employee) => employee.teamName === selectedTeam).length}
+        />
+        <Routes>
+          <Route path='/'
+                 element={<Employees employees={employees}
+                          selectedTeam={selectedTeam}
+                          handleEmployeeCardClick={handleEmployeeCardClick}
+                          handleTeamSelectionChange={handleTeamSelectionChange}
+                        />} >
+            
+          </Route>
+          <Route path='/GroupedTeamMembers' element={<GroupedTeamMembers/>}>
+          </Route>
+        </Routes>  
+        <Footer />
+      </Router>
     </div>
   );
 }
